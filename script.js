ce667e30028b21bddfb6fefc26f93dd815bcb12d
Sudhir Rathod <125038605+sudhirrathod03@users.noreply.github.com>
2026-04-21 15:29:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
 //your JS code here. If required.
+let username = document.getElementById("username")
+let password = document.getElementById("password")
+let check = document.getElementById("checkbox")
+let submit = document.getElementById("submit")
+let form = document.querySelector("form")
+let existingUser = document.getElementById("existing")
+
+window.onload = function(){
+	const savedUser = localStorage.getItem("username")
+	const savedPass = localStorage.getItem("password")
+
+	if(savedUser && savedPass){
+		existingUser.style.display = "block"
+	}
+	else{
+		existingUser.style.display = "none"
+	}
+}
+form.addEventListener("submit",(e)=>{
+	e.preventDefault()
+	// alert(`Logged in as ${username.value}`)
+	if(check.checked){
+	localStorage.setItem("username", username.value)
+	localStorage.setItem("password", password.value)
+	existingBtn.style.display = "block";
+		
+	}
+	else{
+	    localStorage.removeItem("username");
+        localStorage.removeItem("password");
+        existingUser.style.display = "none";
+	}
+})
+
+existingUser.addEventListener("click",()=>{
+	const savedUser = localStorage.getItem("username")
+	if(savedUser){
+		alert(`Logged in as ${savedUser}`)
+	}
+})
+
+
+
+
